59b9fa708ccf2149eab231ee08d94a6428bbaf78
Chisomo Lungu <chisomo@gmail.com>
2017-08-20 22:12:42 +0100

    trying to uninstall and re-install to fix bug

--- babel-scss-workflow/webpack.config.js
+++ babel-scss-workflow/webpack.config.js
@@ -27,7 +27,6 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        exclude: [/node_modules/],
         use: extractPlugin.extract({
           use: ['css-loader', 'sass-loader']
         })
